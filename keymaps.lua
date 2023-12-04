1283a95a807286209c71965d2169e7b77a41c945
Christoffer Lantz <christoffer.lantz@androidbutiken.net>
2023-12-04 20:53:17 +0100

    add grep string

--- keymaps.lua
+++ keymaps.lua
@@ -14,6 +14,7 @@ return {
 		-- find files
 		vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = "Find files" })
 		vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = "Live grep" })
+		vim.keymap.set('n', '<leader>fs', builtin.grep_string, { desc = "Grep string" })
 		vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = "Buffers" })
 		vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = "Help tags" })
 		vim.keymap.set('n', '<leader>fv', builtin.git_files, { desc = "Find git files" })
